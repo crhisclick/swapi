c2431d90a45a173c222378184a990be935d689bf
CrhisClick <crhisclick@gmail.com>
2020-01-22 15:33:32 -0400

    se agrega practica con promesas

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL = 'people/:id'
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id',1)}`
 const opts = { crossDomain: true }
-const onPeopleResponce = function(persona) {
-    console.log(`Hola yo soy ${persona.name}`)
-}
 
 function obtenerPersonaje(id) {
-    const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
-    $.get(url, opts, onPeopleResponce)
+    return new Promise((resolve, reject) => {
+        const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
+        $
+            .get(url, opts, function(data) {
+                resolve(data)
+            })
+            .fail(() => reject(id))
+    })
 }
 
-for (i = 88; i < 99; i++) {
-    obtenerPersonaje(i)
+function onError(id) {
+    console.log(`Sucedio un error al obtener el personaje ${id}`)
 }
 
-
-// $.get(lukeUrl, opts, onPeopleResponce)
+obtenerPersonaje(1)
+    .then(function(personaje) {
+        console.log(`El personaje 1 es ${personaje.name}`)
+    })
+    .catch(onError)

c82f0e6b24afe8fbe878362ab2c55fb31d38a1b7
CrhisClick <crhisclick@gmail.com>
2020-01-23 07:45:03 -0400

    practica con multiples promesas en paralelo

--- index.js
+++ index.js
@@ -17,33 +17,46 @@ function onError(id) {
     console.log(`Sucedio un error al obtener el personaje ${id}`)
 }
 
-obtenerPersonaje(1)
-    .then(personaje => {
-        console.log(`El personaje 1 es ${personaje.name}`)
-        return obtenerPersonaje(2)
-    })
-    .then(personaje => {
-        console.log(`El personaje 2 es ${personaje.name}`)
-        return obtenerPersonaje(3)
-    })
-    .then(personaje => {
-        console.log(`El personaje 3 es ${personaje.name}`)
-        return obtenerPersonaje(4)
-    })
-    .then(personaje => {
-        console.log(`El personaje 4 es ${personaje.name}`)
-        return obtenerPersonaje(5)
-    })
-    .then(personaje => {
-        console.log(`El personaje 5 es ${personaje.name}`)
-        return obtenerPersonaje(6)
-    })
-    .then(personaje => {
-        console.log(`El personaje 6 es ${personaje.name}`)
-        return obtenerPersonaje(7)
-    })
-    .then(personaje => {
-        console.log(`El personaje 7 es ${personaje.name}`)
-    })
+var ids = [1, 2, 3, 4, 5, 6, 7]
+    // var promesas= ids.map(function (id){
+    //     obtenerPersonaje(id)
+    // })
 
-.catch(onError)
+var promesas = ids.map(id => obtenerPersonaje(id))
+Promise
+    .all(promesas)
+    .then(personajes => console.log(personajes))
+    .catch(onError)
+
+
+
+
+// obtenerPersonaje(1)
+//     .then(personaje => {
+//         console.log(`El personaje 1 es ${personaje.name}`)
+//         return obtenerPersonaje(2)
+//     })
+//     .then(personaje => {
+//         console.log(`El personaje 2 es ${personaje.name}`)
+//         return obtenerPersonaje(3)
+//     })
+//     .then(personaje => {
+//         console.log(`El personaje 3 es ${personaje.name}`)
+//         return obtenerPersonaje(4)
+//     })
+//     .then(personaje => {
+//         console.log(`El personaje 4 es ${personaje.name}`)
+//         return obtenerPersonaje(5)
+//     })
+//     .then(personaje => {
+//         console.log(`El personaje 5 es ${personaje.name}`)
+//         return obtenerPersonaje(6)
+//     })
+//     .then(personaje => {
+//         console.log(`El personaje 6 es ${personaje.name}`)
+//         return obtenerPersonaje(7)
+//     })
+//     .then(personaje => {
+//         console.log(`El personaje 7 es ${personaje.name}`)
+//     })
+//     .catch(onError)
